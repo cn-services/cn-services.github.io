e3b97b6960b32ee34fedfd35d329891c6e3526b8
Julian Nonino <noninojulian@gmail.com>
2019-10-31 10:19:03 +0000

    Updating website on master branch

---
+++ precache-manifest.15106ce40d862df2964fe508dc1c85fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a9ed5960835397a37600b7409b8dbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9182bc573d96f3053c7",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8f68cd3c6ad76a6be656",
+    "url": "/static/js/2.5375de00.chunk.js"
+  },
+  {
+    "revision": "c9182bc573d96f3053c7",
+    "url": "/static/js/main.645c9335.chunk.js"
+  },
+  {
+    "revision": "8cbb3c204c280244228c",
+    "url": "/static/js/runtime-main.8527618e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
